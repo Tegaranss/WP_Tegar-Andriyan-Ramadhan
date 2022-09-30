138c25c1178ab3d7a60dbcd2b733c4a33779829a
Tegaranss <tegarandriyan@gmail.com>
2022-09-30 15:22:49 +0700

    Penjumlahan

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -8,4 +8,13 @@ class Contoh1 extends CI_Controller
       Saya suka main 
       Saya dari daerah Jakarta";
  }
+
+
+     public function penjumlahan($n1, $n2)
+     {
+          $this->load->model('Model_latihan1');
+          $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+          echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
+     .$hasil;
+     }
 }

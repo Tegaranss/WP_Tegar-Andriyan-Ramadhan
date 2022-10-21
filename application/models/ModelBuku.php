5a8c1f753018e6cbab851e5a8e4b4d92d6c78e8f
Tegaranss <tegarandriyan@gmail.com>
2022-10-21 18:53:15 +0700

    Latihan pertemuan 7

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -1,67 +1,31 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class ModelBuku extends CI_Model
+class ModelUser extends CI_Model
 {
- //manajemen buku
- public function getBuku()
+ public function simpanData($data = null)
  {
- return $this->db->get('buku');
+    $this->db->insert('user', $data);
  }
- public function bukuWhere($where)
+ public function cekData($where = null)
  {
- return $this->db->get_where('buku', $where);
+ return $this->db->get_where('user', $where);
  }
- public function simpanBuku($data = null)
+ public function getUserWhere($where = null)
  {
- $this->db->insert('buku',$data);
+ return $this->db->get_where('user', $where);
  }
- public function updateBuku($data = null, $where = null)
+ public function cekUserAccess($where = null)
  {
- $this->db->update('buku', $data, $where);
+    $this->db->select('*');
+    $this->db->from('access_menu');
+    $this->db->where($where);
+    return $this->db->get();
  }
- public function hapusBuku($where = null)
- {
- $this->db->delete('buku', $where);
- }
- public function total($field, $where)
- {
- $this->db->select_sum($field);
- if(!empty($where) && count($where) > 0){
- $this->db->where($where);
- }
- $this->db->from('buku');
- return $this->db->get()->row($field);
- }
- 
- //manajemen kategori
- public function getKategori()
- {
- return $this->db->get('kategori');
- }
- public function kategoriWhere($where)
- {
- return $this->db->get_where('kategori', $where);
- }
- public function simpanKategori($data = null)
- {
- $this->db->insert('kategori', $data);
- }
- public function hapusKategori($where = null)
- {
- $this->db->delete('kategori', $where);
- }
- public function updateKategori($where = null, $data = null)
- {
- $this->db->update('kategori', $data, $where);
- }
- //join
- public function joinKategoriBuku($where)
- {
- $this->db->select('buku.id_kategori,kategori.kategori');
- $this->db->from('buku');
- $this->db->join('kategori','kategori.id = 
-buku.id_kategori');
- $this->db->where($where);
- return $this->db->get();
+    public function getUserLimit()
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->limit(10, 0);
+        return $this->db->get();
  }
 }
